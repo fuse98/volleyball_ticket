d54867622596ec609921db304a3ee667d0c93bc6
Moein <moein.f.ebrahimy@gmail.com>
2024-12-09 16:30:09 +0330

    feat: add register api

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import UserRegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', UserRegistrationView.as_view(), name='user_registration'),
 ]
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    is_stadium_admin = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=11, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.phone_number
